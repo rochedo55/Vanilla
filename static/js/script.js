e69e9d8c472c581ea330fbb35cee788322427c87
Gabriel Rocha de Souza <43376647+rochedo55@users.noreply.github.com>
2020-10-06 16:07:24 -0300

    adicionando o modal para adicionar despesas e receitas, ainda nao implementado a insercao do conteudo do input em um card especifico

--- static/js/script.js
+++ static/js/script.js
@@ -4,3 +4,32 @@ function showAddTransaction() {
     document.querySelector('.add-expense').classList.toggle('show');
     document.querySelector('.add-revenue').classList.toggle('show');
 }
+
+const modal1 = document.getElementById("myModal1")
+const modal2 = document.getElementById("myModal2")
+
+const span1 = document.getElementsByClassName("close")[0];
+const span2 = document.getElementsByClassName("close")[1];
+
+function openModalRevenue(){
+    modal1.style.display = "block";
+}
+
+function openModalExpense(){
+    modal2.style.display = "block";
+}
+
+span1.onclick = function() {
+    modal1.style.display = "none";
+}
+
+span2.onclick = function() {
+    modal2.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal1 || event.target == modal2) {
+      modal1.style.display = "none";
+      modal2.style.display = "none";
+    }
+}
